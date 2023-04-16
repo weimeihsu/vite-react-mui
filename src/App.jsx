e7d8ce9928264e32f7f623f12b5139a31b50b40d
weimeihsu <weimei0928@gmail.com>
2023-04-16 14:18:11 +0800

    add click button back

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@
 import CssBaseline from '@mui/material/CssBaseline'
 import SideBar from './components/SideBar'
 import Boxes from './components/Boxes'
+import ButtonClick from './components/ButtonClick'
 
 function App() {
   // const [theme, colorMode] = useMode()
@@ -13,7 +14,7 @@ function App() {
           <main className='content'>
           <SideBar/>
           <Boxes/>
-          
+          <ButtonClick/>
           <h1>Vite + React + Mui</h1>
           </main>
         </div>
