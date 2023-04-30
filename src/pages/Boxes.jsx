7666ab6859ec085338156659da1fb78b3f7f4eb7
weimeihsu <weimei0928@gmail.com>
2023-04-30 22:27:41 +0800

    card sx style

--- src/pages/Boxes.jsx
+++ src/pages/Boxes.jsx
@@ -1,26 +1,39 @@
 import { useState } from "react"
 import DeleteIcon from '@mui/icons-material/Delete';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Chip from '@mui/material/Chip'
 
 // use state to modify data
 const Boxes = () => {
     const [boxes, setBoxValue] =useState([
-        {id:1, title:'React', href:'#'},
-        {id:2, title:'Demo', href:'#'},
-        {id:3, title:'Vue', href:'#'},
+        {title:'Snow White', type:'Cartoon'},
+        {title:'The Secret Garden', type:'Comedy'},
+        {title:'Lion King', type:'Drama'},
+        {title:'Little Mermaid', type:'Thriller'},
+        {title:'Spider man', type:'Action'},
+        {title:'What Happened to Wed', type:'Action'},
     ])
     return ( 
         <>
         <h1>Boxes CRUD case</h1>
-        <div className="boxContainer">
-            useState list the value can be reactively changed
+        <Stack  direction="row"
+                flexWrap="wrap">
             {boxes.map((boxItem, idx)=>(
-                <div className="box" key={idx}>
-                <a className="card"  href={boxItem.href}>{boxItem.title}</a> 
-                <DeleteIcon/>
-                </div>     
+                <Box variant="outlined" className="box" key={idx} sx={{ p: 2,m:1,backgroundColor: 'primary.dark',
+                borderRadius:'4px',
+                cursor:'pointer',
+                '&:hover': {
+                  backgroundColor: 'primary.dark',
+                  opacity: [0.9, 0.8, 0.7],
+                },}}>
+                <Chip label={boxItem.type} size="small"/>
+                <h3>{boxItem.title}</h3> 
+                
+                <DeleteIcon sx={{ fontSize: 16 }}/>
+                </Box>   
         ))}
-            
-        </div>
+        </Stack>    
         </>
      )
 }
